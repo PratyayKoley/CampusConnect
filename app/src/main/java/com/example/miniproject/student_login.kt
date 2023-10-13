4a38fcbb9d638ac4ee609c4d5e13df026c33dfa7
Pratyay <pratyaykoley@gmail.com>
2023-10-13 18:14:00 +0530

    First Version

---
+++ app/src/main/java/com/example/miniproject/student_login.kt
@@ -0,0 +1,11 @@
+package com.example.miniproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class student_login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_login)
+    }
+}
