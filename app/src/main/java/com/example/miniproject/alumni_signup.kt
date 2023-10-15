7eb784fce92c1cbefe1d4a27608cbaa426ef024f
Aadil Attar <aadilattar104@gmail.com>
2023-10-16 01:10:03 +0530

    FirstVersion

--- app/src/main/java/com/example/miniproject/alumni_signup.kt
+++ app/src/main/java/com/example/miniproject/alumni_signup.kt
@@ -3,8 +3,9 @@ package com.example.miniproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.google.firebase.auth.FirebaseAuth
-lateinit var button5:Button
+lateinit var button5: Button
 class alumni_signup : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
