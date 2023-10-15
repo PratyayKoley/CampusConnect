986104ba8d7bd2957dd2f78dfbbf439a56a9261c
Aryan Daripkar <aryandaripkar30@gmail.com>
2023-10-15 17:15:41 +0530

    .

--- app/src/main/java/com/example/miniproject/alumni_signup.kt
+++ app/src/main/java/com/example/miniproject/alumni_signup.kt
@@ -2,10 +2,13 @@ package com.example.miniproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class alumni_signup : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alumni_signup)
+
     }
 }
