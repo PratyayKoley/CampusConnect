6d39d3f3f53f333463268ea52fb5cb58b083be2c
Aadil Attar <aadilattar104@gmail.com>
2023-10-16 00:49:47 +0530

    FirstVersion

--- app/src/main/java/com/example/miniproject/student_login.kt
+++ app/src/main/java/com/example/miniproject/student_login.kt
@@ -1,14 +1,24 @@
 package com.example.miniproject
-
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.google.firebase.auth.FirebaseAuth
+lateinit var button3: Button
 
 class student_login : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_login)
+        //finding button student login
+        button3=findViewById(R.id.button3)
+        //making a function to handle on click
+        button3.setOnClickListener {
+            //creating intent
+            intent= Intent(this,student_login::class.java)
+            startActivity(intent)
+        }
 
         auth = FirebaseAuth.getInstance()
 
