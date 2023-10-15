626d76c55d4af2eba615de82045696faa4faec1a
Aryan Daripkar <aryandaripkar30@gmail.com>
2023-10-15 15:34:23 +0530

    .

--- app/src/main/java/com/example/miniproject/student_login.kt
+++ app/src/main/java/com/example/miniproject/student_login.kt
@@ -2,10 +2,19 @@ package com.example.miniproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class student_login : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_login)
+
+        auth = FirebaseAuth.getInstance()
+
+
+
+
+
     }
 }
