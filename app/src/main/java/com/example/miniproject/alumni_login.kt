49ad42b5ec26306b1a7cb84de3fa80dedba38e28
Aadil Attar <aadilattar104@gmail.com>
2023-10-16 20:33:00 +0530

    FirstVersion

--- app/src/main/java/com/example/miniproject/alumni_login.kt
+++ app/src/main/java/com/example/miniproject/alumni_login.kt
@@ -2,12 +2,22 @@ package com.example.miniproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import com.google.android.gms.common.SignInButton.ButtonSize
 import com.google.firebase.auth.FirebaseAuth
 
 class alumni_login : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private lateinit var editTextTextEmailAddress:EditText
+    private lateinit var Alu_Pass: EditText
+    private lateinit var Login: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alumni_login)
+        auth= FirebaseAuth.getInstance()
+        editTextTextEmailAddress= findViewById(R.id.editTextTextEmailAddress)
+        Alu_Pass=findViewById(R.id.Alu_Pass)
+        Login=findViewById(R.id.Login)
+
     }
 }
