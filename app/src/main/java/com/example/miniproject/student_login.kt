10a6c9ea8421099bdd2b2114a5079f73db149a39
Aryan Daripkar <aryandaripkar30@gmail.com>
2023-10-16 13:07:51 +0530

    Login

--- app/src/main/java/com/example/miniproject/student_login.kt
+++ app/src/main/java/com/example/miniproject/student_login.kt
@@ -3,28 +3,53 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
-lateinit var button3: Button
+
 
 class student_login : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private lateinit var Stu_Email: EditText
+    private lateinit var Password: EditText
+    private lateinit var Login: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_login)
-        //finding button student login
-        button3=findViewById(R.id.button3)
-        //making a function to handle on click
-        button3.setOnClickListener {
-            //creating intent
-            intent= Intent(this,Main_Forum::class.java)
-            startActivity(intent)
-        }
 
         auth = FirebaseAuth.getInstance()
 
+        Login.setOnClickListener{
+            if(checking()){
+                val email=Stu_Email.text.toString()
+                val password=Password.text.toString()
+                auth.signInWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(this){
+                        task ->
+                        if(task.isSuccessful){
+                            Toast.makeText(this,"Login Successfull ",Toast.LENGTH_LONG).show()
 
+                        }
+                        else
+                        {
+                            Toast.makeText(this,"Wrong Credentials",Toast.LENGTH_LONG).show()
+                        }
+                    }
 
+            }
+            else
+            {
+                Toast.makeText(this,"Enter the details",Toast.LENGTH_LONG).show()
+            }
+        }
 
-
+    }
+    private fun checking():Boolean
+    {
+        if(Stu_Email.text.toString().trim().isEmpty() || Password.text.toString().trim().isEmpty())
+        {
+            return true
+        }
+            return false
     }
 }
