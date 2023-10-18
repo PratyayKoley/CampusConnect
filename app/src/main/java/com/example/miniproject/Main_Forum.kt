5ed3b35da840bc6300f2eb7783fd0453768892b1
Pratyay <pratyaykoley@gmail.com>
2023-10-18 17:16:16 +0530

    Logout

--- app/src/main/java/com/example/miniproject/Main_Forum.kt
+++ app/src/main/java/com/example/miniproject/Main_Forum.kt
@@ -7,10 +7,14 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.firebase.ui.database.FirebaseRecyclerAdapter
-
 import androidx.recyclerview.widget.LinearLayoutManager
 import android.widget.ProgressBar
 import android.widget.LinearLayout
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.ImageView
+import android.widget.PopupMenu
 
 class Main_Forum : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
@@ -20,11 +24,35 @@ class Main_Forum : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_forum)
 
+        val moreImageView: ImageView = findViewById(R.id.More)
+
+        moreImageView.setOnClickListener{  view ->
+            showPopupMenu(view)
+        }
         auth = FirebaseAuth.getInstance()
         FirebaseDatabase.getInstance().setPersistenceEnabled(true)
         database =
             FirebaseDatabase.getInstance("https://mini-project-62a72-default-rtdb.asia-southeast1.firebasedatabase.app")
                 .getReference("messages")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_item,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId)
+        {
+            R.id.menu_item ->{
+
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+    private fun showPopupMenu(view: View?) {
+
     }
 }
 
