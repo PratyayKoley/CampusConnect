add4f1b825aebcff985f3069666bfd259a692226
PratyayKoley <pratyaykoley@gmail.com>
2024-03-18 01:14:45 +0530

    Dark Mode On

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,5 +67,6 @@ dependencies {
     implementation ("androidx.appcompat:appcompat:1.3.0")
     implementation ("com.google.android.material:material:1.5.0")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation ("com.google.android.material:material:1.4.0")
 
 }
